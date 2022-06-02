bf265567e2f8cd2197d88c3818b60c1bceda91b4
unknown <samirsingh.2000.ss@gmail.com>
2022-06-02 18:03:35 +0530

    loader logo change

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { motion } from "framer-motion";
+import logo from "../assets/Images/Logo.jpg";
 
 const Container = styled(motion.div)`
   position: absolute;
@@ -112,31 +113,39 @@ const Loader = () => {
         duration: 2,
       }}
     >
-      <svg
+      {/* <svg
         xmlns="http://www.w3.org/2000/svg"
         enableBackground="new 0 0 24 24"
         height="48px"
         viewBox="0 0 24 24"
         width="48px"
         fill="none"
-      >
-        <g></g>
-        <g>
-          <motion.path
+      > */}
+      {/* <g></g> */}
+      {/* <g> */}
+      <img
+        style={{
+          width: "100%",
+          height: "100%",
+        }}
+        src={logo}
+        alt="logo"
+      ></img>
+      {/* <motion.path
             variants={pathVariants}
             initial="hidden"
             animate="visible"
             d="M12,17.27L18.18,21l-1.64-7.03L22,9.24l-7.19-0.61L12,2L9.19,8.63L2,9.24l5.46,4.73L5.82,21L12,17.27z"
-          />
-        </g>
-      </svg>
-      <Text variants={textVariants} initial="hidden" animate="visible">
+          /> */}
+      {/* </g> */}
+      {/* </svg> */}
+      {/* <Text variants={textVariants} initial="hidden" animate="visible">
         Mukhi Garments
       </Text>
 
       <Text2 variants={textVariants} initial="hidden" animate="visible">
         All types of Ladies Garments
-      </Text2>
+      </Text2> */}
     </Container>
   );
 };
